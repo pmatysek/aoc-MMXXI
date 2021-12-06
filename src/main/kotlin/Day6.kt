bd09368bc7cee49d645a93ab8de90b25e9e2beae
pmatysek <pawel.matysek@comarch.pl>
2021-12-06 09:14:48 +0100

    Day5

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,30 @@
+const val NUMBER_OF_DAYS_TO_SIMULATE = 256
+
+fun main() {
+    var lanternfishes = "day6"
+        .getInputLines()
+        .first()
+        .split(",")
+        .map { it.toInt() }
+        .groupingBy { it }
+        .eachCount()
+        .mapValues { it.value.toLong() }
+
+    for (i in 0 until NUMBER_OF_DAYS_TO_SIMULATE) {
+        lanternfishes = lanternfishes.simulateLaternfishDay()
+    }
+
+    println("Number of fishes after $NUMBER_OF_DAYS_TO_SIMULATE is: ${lanternfishes.values.sum()}")
+}
+
+
+fun Map<Int, Long>.simulateLaternfishDay(): Map<Int, Long> {
+    val numberOfNewKids = this[0] ?: 0
+    val newFishesMap = mutableMapOf<Int, Long>()
+    for(i in 0..7){
+        newFishesMap[i] = this[i + 1] ?: 0
+    }
+    newFishesMap[8] = numberOfNewKids
+    newFishesMap[6] =  numberOfNewKids + (newFishesMap[6] ?: 0)
+    return newFishesMap
+}
