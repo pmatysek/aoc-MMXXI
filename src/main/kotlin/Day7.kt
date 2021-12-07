25207e9ca2934c915b68155b0e1ad4bb18c04ff7
pmatysek <pawel.matysek@comarch.pl>
2021-12-07 13:33:05 +0100

    Day7

---
+++ src/main/kotlin/Day7.kt
@@ -0,0 +1,19 @@
+import kotlin.math.abs
+
+fun main() {
+    val positions = "day7".getInputLines().first().split(",").map { it.toInt() }
+    val fuelSums = (positions.minOrNull()!!..positions.maxOrNull()!!).associateWith { targetPosition ->
+        positions.sumOf { currentPosition -> abs(currentPosition - targetPosition) }
+    }
+    println("They have to spend ${fuelSums.minOf { it.value }} fuel")
+
+    val fuelSums2 = (positions.minOrNull()!!..positions.maxOrNull()!!).associateWith { targetPosition ->
+        positions.sumOf { currentPosition -> calculateFuel(currentPosition, targetPosition) }
+    }
+    println("They have to spend ${fuelSums2.minOf { it.value }} fuel")
+}
+
+private fun calculateFuel(currentPosition: Int, targetPosition: Int): Int {
+    val n =abs(currentPosition - targetPosition)
+    return ((1 + n) * n)/2
+}
